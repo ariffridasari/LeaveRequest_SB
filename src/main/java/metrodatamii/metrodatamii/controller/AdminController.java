fe7d81f8c13e30720b37d43ee94ca6b18e9baa95
Arif_Fridasari <>
2019-07-09 09:17:49 +0700

    Arif_Fridasari The Juragan Lele

--- src/main/java/metrodatamii/metrodatamii/controller/AdminController.java
+++ src/main/java/metrodatamii/metrodatamii/controller/AdminController.java
@@ -269,33 +269,33 @@ public class AdminController {
         return "redirect:/role";
     }
 
-    @GetMapping("/403")
-    public String error403() {
-        return "error/404";
-    }
-
-    @GetMapping("/500")
-    public String error500() {
-        return "error/500";
-    }
-
-    @RequestMapping(value = "/error", method = RequestMethod.GET)
-    public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
-
-            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error/404";
-            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error/500";
-            } else if (statusCode == HttpStatus.BAD_REQUEST.value()) {
-                return "error/400";
-            }
-        }
-        return "error/error";
-    }
+//    @GetMapping("/403")
+//    public String error403() {
+//        return "error/404";
+//    }
+//
+//    @GetMapping("/500")
+//    public String error500() {
+//        return "error/500";
+//    }
+//
+//    @RequestMapping(value = "/error", method = RequestMethod.GET)
+//    public String handleError(HttpServletRequest request) {
+//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//
+//        if (status != null) {
+//            Integer statusCode = Integer.valueOf(status.toString());
+//
+//            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+//                return "error/404";
+//            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+//                return "error/500";
+//            } else if (statusCode == HttpStatus.BAD_REQUEST.value()) {
+//                return "error/400";
+//            }
+//        }
+//        return "error/error";
+//    }
 
     @GetMapping("/home/profile")
     public String profile(Model model) {
